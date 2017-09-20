d3f79fda68704ec4acaf0e337967cca75d3ee45c
olivar3 <olivar3@outlook.com>
2017-09-19 23:51:00 -0500

    Getting the controll information to show up

--- TheChromium/Controllers/MembersController.cs
+++ TheChromium/Controllers/MembersController.cs
@@ -3,15 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TheChromium.Models;
 
 namespace TheChromium.Controllers
 {
     public class MembersController : Controller
     {
-        // GET: Members
+        // Create: Member
+        public ActionResult Create()
+        {
+            return View();
+        }
+        // Delete:Member
+        public ActionResult Delete()
+        {
+            return View();
+        }
+        // ShowDetails:Member
+        public ActionResult Details()
+        {
+            return View();
+        }
+        //ShowDetails:Edit:Member
+        public ActionResult Edit()
+        {
+            return View();
+        }
+        //Get:Members:HomePage
         public ActionResult Index()
         {
             return View();
         }
+
     }
 }
