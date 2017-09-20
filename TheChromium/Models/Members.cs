f2a60ed7e05dd289162c5189a05957d4c5489bf5
ZackZielinski <Zackzielinski0@gmail.com>
2017-09-20 08:18:32 -0500

    recorrecting somw work within the members section

---
+++ TheChromium/Models/Members.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TheChromium.Models
+{
+    public class Members
+    {
+        [Key]
+        public int id { get; set; }
+
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+
+
+
+    }
+}
