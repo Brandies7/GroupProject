9a93bf6e434cbd9bba415639c2ae9a28ad0aabfd
hegotgame <jgill@uwm.edu>
2017-09-21 11:57:43 -0500

    FB is now linked

---
+++ TheChromium/FBDefault.aspx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace TheChromium
+{
+    public partial class FBDefault : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            HyperLink1.NavigateUrl = "https://www.facebook.com/v2.4/dialog/oauth/?client_id=" + ConfigurationManager.AppSettings["FacebookAppId"] + "&redirect_uri=http://" + Request.ServerVariables["SERVER_NAME"] + ":" + Request.ServerVariables["SERVER_PORT"] + "/fbaccount/user.aspx&response_type=code&state=1";
+            HyperLink1.Text = "Login with Facebook";
+        }
+    }
+}
